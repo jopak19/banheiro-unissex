20f3a218b78e54c749b03a33179baf5d40b95f2e
João Paulo <41856004+jopak19@users.noreply.github.com>
2022-12-03 18:35:18 -0300

    ajuste na execução do programa

--- src/main/java/com/mycompany/banheiro/unissex/BanheiroUnissex.java
+++ src/main/java/com/mycompany/banheiro/unissex/BanheiroUnissex.java
@@ -4,44 +4,30 @@ import java.util.Scanner;
 public class BanheiroUnissex {
 
 
-    
+    static final int CAPACIDADE = 4;
     /** 
      * @param args Argumentos de inicialização
      */
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite 's' se quiser determinar a quantidade de vagas " +
-                "no banheiro ou qualquer outra tecla para usar o padrão de 4 vagas: ");
-        String opcao = scanner.nextLine();
-        int capacidade = 4;
-        if(opcao.equals("s")){
-            System.out.println("Digite a quantidade de vagas no banheiro: ");
-            try {
-                capacidade = scanner.nextInt();
-            } catch (Exception e) {
-                System.out.println("Valor inválido, será utilizado o valor padrão de 4 vagas");
-            }
-        }
 
-        FilaBanheiro filaBanheiro = new FilaBanheiro(capacidade);
+        FilaBanheiro filaBanheiro = new FilaBanheiro(CAPACIDADE);
         filaBanheiro.iniciarFila();
         filaBanheiro.abrirBanheiro();
-
-        //se não colocar uma espera o programa encerra antes de executar
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
 
 
         System.out.println("\n\n\nExecutando, para parar a execução " +
                 "a qualquer momento digite qualquer tecla e aperte enter\n\n\n");
+        Scanner scanner = new Scanner(System.in);
 
         scanner.nextLine();
         filaBanheiro.setExecutando(false);
-
+      
+        
     }
+        
+        
+
+    
 
 }
